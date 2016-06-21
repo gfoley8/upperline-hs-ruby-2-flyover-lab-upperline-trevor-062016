dfd15ed1ea272f7402ee515bb38022a705d2db2d
Grace Foley <gfoley18@spenceschool.org>
2016-06-21 13:38:03 +0000

    Done.

--- flyover.rb
+++ flyover.rb
@@ -1,1 +1,15 @@
-## Code your solution here.
+def flyOver(country)
+  puts "We are now flying over #{country}"
+end
+
+flyOver("New York")
+flyOver("Pennsylvania")
+flyOver("Ohio")
+flyOver("Indiana")
+flyOver("Illinois")
+flyOver("Missouri")
+flyOver("Kansas")
+flyOver("Colorado")
+flyOver("Utah")
+flyOver("Nevada")
+flyOver("California")
